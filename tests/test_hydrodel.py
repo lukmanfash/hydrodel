2b8800a20e8000f6232a7a823080a7e8eaafa735
Lukman Fashina <lukmanfashina@yahoo.com>
2023-03-08 00:18:58 -0500

    initial commit

---
+++ tests/test_hydrodel.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `hydrodel` package."""
+
+
+import unittest
+
+from hydrodel import hydrodel
+
+
+class TestHydrodel(unittest.TestCase):
+    """Tests for `hydrodel` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
